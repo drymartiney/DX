c8192db2f2a6e221eba31375d50b194f1da96e82
ChenFeiYun <541628730@qq.com>
2021-08-30 17:08:26 +0800

    1

---
+++ src/main/java/com/dx/panxie/PanxieApplication.java
@@ -0,0 +1,13 @@
+package com.dx.panxie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanxieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanxieApplication.class, args);
+    }
+
+}
